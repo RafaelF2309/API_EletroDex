84fde0b0f37117555df3c03d5aeccd640b71b1f6
Rafifa <rafael.f.silva55@aluno.senai.br>
2026-08-10 07:55:12 -0300

    implementação do multer nas rotas necessárias

--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const UsuarioController = require('../controllers/UsuarioController');
+const upload = require('../config/multer');
 
 // GET /usuarios
 router.get('/', UsuarioController.listar);
@@ -8,11 +9,11 @@ router.get('/', UsuarioController.listar);
 // GET /usuarios/:id
 router.get('/:id', UsuarioController.buscarPorId);
 
-// POST /usuarios
-router.post('/', UsuarioController.criar);
+// POST /usuarios (campo: foto)
+router.post('/', upload.single('foto'), UsuarioController.criar);
 
-// PATCH /usuarios/:id
-router.patch('/:id', UsuarioController.atualizar);
+// PATCH /usuarios/:id (campo: foto)
+router.patch('/:id', upload.single('foto'), UsuarioController.atualizar);
 
 // DELETE /usuarios/:id
 router.delete('/:id', UsuarioController.remover);
